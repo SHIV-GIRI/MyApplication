de3661e071ff4454b2f81bb9582bb29b230e4682
shivgiri <shivgirir@gmail.com>
2021-05-06 23:03:34 +0530

    Sucessfully added

---
+++ SayHello/src/main/java/com/demo/SayHelloApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayHelloApplication.class, args);
+	}
+
+}
